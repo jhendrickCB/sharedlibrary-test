59f9516fa2581a874d0af9d735a021c0ad243b3a
jhendrick <jhendrick@users.noreply.github.com>
2018-10-01 15:59:54 -0700

    Create pipeline-template.groovy

---
+++ vars/pipeline-template.groovy
@@ -0,0 +1,24 @@
+def call(Map pipelineParams) {
+
+  pipeline {
+    agent any
+    stages {
+      stage('Build') {
+        steps {
+          echo "Building application"
+        }
+      } 
+      stage('Deploy') {
+        steps {
+          echo "Deploy application"
+        }
+      }
+      stage('Test') {
+        steps {
+          echo "Testing application"
+        }
+      }
+    }
+  }
+
+}
